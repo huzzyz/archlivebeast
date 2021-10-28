042844edd0514cb8fbd2fda7c2c2bc02dfebef7c
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-28 12:03:53 +0400

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -3,7 +3,7 @@ app_list="xorg sddm plasma breeze-gtk kdeconnect kde-gtk-config khotkeys kinfoce
 plasma-disks plasma-nm plasma-pa powerdevil print-manager sddm-kcm solid xsettingsd google-chrome teamviewer discover smplayer smplayer-themes
 bleachbit nasc ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free 
 stacer-bin teamviewer backintime tesseract tesseract-data-eng leptonica xdotool libinput-gestures plasma-browser-integration 
-nemo nemo-fileroller nemo-preview nemo-share wget gthumb gedit packagekit-qt5 corectrl fish pkgfile ttf-dejavu powerline-fonts ttf-ms-win11"
+nemo nemo-fileroller nemo-preview nemo-share wget gthumb gedit packagekit-qt5 corectrl fish pkgfile ttf-dejavu powerline-fonts ttf-ms-win11 skanlite"
 
 yay -Syyu $app_list --noconfirm
 
